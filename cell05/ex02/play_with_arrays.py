f7754e0e247ff6cab2606fc53aa73d0ae95f5456
satangKung <paphawit145@gmail.com>
2026-09-21 15:58:50 +0700

    cell05 ex02

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,9 @@
+org_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for i in org_array :
+    if i > 5 :
+        new_array.append(i + 2)
+
+print("Original array :",org_array)
+print("New array :",new_array)
