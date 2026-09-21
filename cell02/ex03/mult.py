e9a09b7f08ed12f3bb758383afbc2d737cab3e78
satangKung <paphawit145@gmail.com>
2026-09-21 13:45:59 +0700

    cell02 ex03

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+result = num1 * num2
+
+print(f"{num1} x {num2} = {result}")
+
+if result < 0:
+    print("This result is negative.")
+elif result > 0:
+    print("This result is positive.")
+else:
+    print("The result is positive and negative.")
